39523da8fc7b2a49f291c61364701a8e782da347
Onasanya Tunde <Onasanyatunde67@gmail.com>
2019-10-16 22:07:32 +0100

    Added the regEX

--- docufix.js
+++ docufix.js
@@ -1,28 +1,33 @@
+
 $('#compare').on('click', function (e) {
 
 
     e.preventDefault();
     console.log('Hello');
-    
+    let separators = [' ', '\\\+', '-', '\\\(', '\\\)','\\*', '/', '\\\.', '\\\?'];
     let firstString = $('#input1').val()
     let secondString = $('#input2').val()
-    let firstStringSet = new Set(firstString.split(' '))
-    let secondStringSet = new Set(secondString.split(' '))
+    let firstStringSet = new Set(firstString.split(new RegExp(separators.join('|'), 'g')))
+    let secondStringSet = new Set(secondString.split(new RegExp(separators.join('|'), 'g')))
 
+    console.log(secondStringSet);
+    
 
-    let firstStringArray = firstString.split(' ')
-    let secondStringArray = secondString.split(' ')
+    let firstStringArray = firstString.split(new RegExp(separators.join('|'), 'g'))
+    console.log(firstStringArray);
+    
+    let secondStringArray = secondString.split(new RegExp(separators.join('|'), 'g'))
     
     secondStringSet.forEach( word => {
         let resultA = firstStringArray.filter(newWord => newWord === word  );
         let resultB = secondStringArray.filter( newWord => newWord === word );
-        if (resultB != 0 || resultA != 0 ){
+        if (resultB != 0 && resultA != 0 ){
         console.log( `${word} appeared in First document ${resultA.length} time and it appeared in Second document ${resultB.length} times`);
       
       
         $('#result').append(
             
-            `${word} appeared in First document ${resultA.length} time and it appeared in Second document ${resultB.length} times`)
+            `${word} appeared in First document ${resultA.length} time and it appeared in Second document ${resultB.length} times <br>`)
     
     }
     })
